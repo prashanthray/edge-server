b3f20c52614a38e7f44d94479e1629aaff1ca50b
prashanth.rayasa <prashanth.rayasa@accenture.com>
2020-05-13 11:47:48 -0600

    Changes to add Eureka server.

--- edge-service/src/main/java/com/example/zuul/EdgeServiceApplication.java
+++ edge-service/src/main/java/com/example/zuul/EdgeServiceApplication.java
@@ -2,6 +2,7 @@ package com.example.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import com.netflix.zuul.context.RequestContext;
@@ -10,6 +11,7 @@ import com.example.zuul.SimplePostFilter;
 import com.example.zuul.SimplePreFilter;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableZuulProxy
 public class EdgeServiceApplication {
 
